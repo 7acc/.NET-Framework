afca63ff65e477820e067e579097a00ff371c436
7acc <jacc321@gmail.com>
2016-09-09 14:31:19 +0200

    fredag

---
+++ Labbar/testconsolforms/testconsolforms/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace testconsolforms
+{
+    class MainWindow : Form
+    {
+        private MenuStrip mnuMainMenu = new MenuStrip();
+        private ToolStripMenuItem mnuFile = new ToolStripMenuItem();
+        private ToolStripMenuItem mnuExit = new ToolStripMenuItem();
+
+        public MainWindow() { }
+        public MainWindow(string title, int height, int width)
+        {
+            Text = title;
+            Width = width;
+            Height = height;
+            CenterToScreen();
+
+            BuildMenuSystem();
+        }
+        private void BuildMenuSystem()
+        {
+            mnuFile.Text = "&File";
+            mnuMainMenu.Items.Add(mnuFile);
+
+
+            mnuExit.Text = "&Exit";
+            mnuFile.DropDownItems.Add(mnuExit);
+            mnuExit.Click += (o, s) => Application.Exit();
+
+            Controls.Add(this.mnuMainMenu);
+            MainMenuStrip = this.MainMenuStrip;
+        }
+
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Run(new MainWindow("MyLittleWindow", 300, 500));
+        }
+    }
+
+
+}
---
+++ Övningar/SimpleLambda/SimpleLambda/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLambda
+{
+    class Program
+    {
+        delegate void testDelegate(string s);
+        static List<int> list;
+        static void Main(string[] args)
+        {
+            list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            DelegateThingy();
+            TraditionalDelegateSyntax();
+            AnonymousMethodSyntax();
+            LambdaExpressionSyntax();
+        }
+        static void PrintEvenNumbers(List<int> evenNumbers)
+        {
+            Console.WriteLine("Even Numbers!");
+            evenNumbers.ForEach(x => Console.WriteLine(x));
+            Console.ReadKey();
+        }
+        static bool IsEvenNumber(int i)
+        {
+            return (i % 2) == 0;
+        }
+        static void TraditionalDelegateSyntax()
+        {
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+            PrintEvenNumbers(evenNumbers);
+        }
+
+        static void AnonymousMethodSyntax()
+        {
+            List<int> evenNumbers = list.FindAll(delegate (int i)
+            { return (i % 2) == 0; });
+
+            PrintEvenNumbers(evenNumbers);
+        }
+
+        static void LambdaExpressionSyntax()
+        {
+            List<int> evenNnumbers = list.FindAll((int i) => (i % 2) == 0);
+            PrintEvenNumbers(evenNnumbers);
+        }
+
+        static void DelegateThingy()
+        {
+            testDelegate mydelegate = n => { string s = n + " " + "World"; Console.WriteLine(s); };
+            mydelegate("hello");
+        }
+
+    }
+}
